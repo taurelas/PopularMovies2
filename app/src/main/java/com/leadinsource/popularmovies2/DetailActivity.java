d89bda8c1441307737008f0c9475bf7160ccb079
Matt <mbeimcik@gmail.com>
2018-02-28 16:16:12 +0100

    dummy favorite button

--- app/src/main/java/com/leadinsource/popularmovies2/DetailActivity.java
+++ app/src/main/java/com/leadinsource/popularmovies2/DetailActivity.java
@@ -1,9 +1,16 @@
 package com.leadinsource.popularmovies2;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.leadinsource.popularmovies2.databinding.ActivityDetailBinding;
 import com.leadinsource.popularmovies2.model.Movie;
@@ -12,6 +19,10 @@ import com.squareup.picasso.Picasso;
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_MOVIE = "EXTRA_MOVIE";
+    private MenuItem addToFavorites;
+    private Menu menu;
+
+    DetailActivityViewModel viewModel;
 
 
     @Override
@@ -19,6 +30,8 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         ActivityDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+
+        viewModel = ViewModelProviders.of(this).get(DetailActivityViewModel.class);
 
         Intent intent = getIntent();
 
@@ -32,4 +45,44 @@ public class DetailActivity extends AppCompatActivity {
             binding.tvSynopsis.setText(movie.overview);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+
+        this.menu = menu;
+        addToFavorites = menu.findItem(R.id.favorite);
+
+        observeIsFavorite();
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item==addToFavorites) {
+            viewModel.switchFavorite();
+        }
+
+        if(item.getItemId()==android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return true;
+    }
+
+    private void observeIsFavorite() {
+        viewModel.isFavorite().observe(this, isFavorite -> {
+            if(isFavorite) {
+                addToFavorites.setIcon(android.R.drawable.star_big_on);
+
+            } else {
+                addToFavorites.setIcon(android.R.drawable.star_big_off);
+
+            }
+        });
+    }
+
+
 }
